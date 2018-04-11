559f5735bd87385bf304f7ba53d63a646208c582
Presley Lupon <presleylupon@gmail.com>
2018-04-11 11:54:07 +0200

    ajout phrase aceuille

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
-<?php
-
-echo '<h1> Hello, World<h1>';
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Document</title>
+  </head>
+  <body>
+    <h1> une vrais page :D</h1>
+  </body>
+</html>
